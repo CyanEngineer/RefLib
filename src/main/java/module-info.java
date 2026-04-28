b021f1f64b7a6cfb3081e08eecd087d726a62cc7
CyanEngineer <hello@cyaneer.com>
2026-04-28 22:35:10 +0200

    Clean up unused modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,10 @@
 module com.cyaneer.reflib {
     requires java.management;
     
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
     requires javafx.base;
+    requires javafx.controls;
+    requires transitive javafx.graphics;
 
-    opens com.cyaneer.reflib to javafx.fxml;
     exports com.cyaneer.reflib;
     exports com.cyaneer.reflib.model;
 }
